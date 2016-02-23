77e813267c710839df0f437fdb991122b5cd72de
Dharam Maniar <dharammaniar@hotmail.com>
2016-02-23 10:32:15 -0500

    Client Side Dev: Done with Header Routing

---
+++ public/assignment/config.js
@@ -0,0 +1,25 @@
+(function(){
+    angular
+        .module('FormBuilderApp')
+        .config(function($routeProvider) {
+            $routeProvider
+                .when("/", {
+                    templateUrl: "views/home/home.view.html"
+                })
+                .when("/register", {
+                    templateUrl: "views/users/register.view.html"
+                })
+                .when("/login", {
+                    templateUrl: "views/users/login.view.html"
+                })
+                .when("/profile", {
+                    templateUrl: "views/users/profile.view.html"
+                })
+                .when("/admin", {
+                    templateUrl: "views/admin/admin.view.html"
+                })
+                .otherwise({
+                    redirectsTo: "/"
+                });
+        });
+})();
