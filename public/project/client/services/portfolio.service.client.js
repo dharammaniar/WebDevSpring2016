69770960beb867f3636638080c23175cd44159ff
Dharam Maniar <dharammaniar@hotmail.com>
2016-04-10 19:49:22 -0400

    Project: Move getPortfolio code to PortfolioService

--- public/project/client/services/portfolio.service.client.js
+++ public/project/client/services/portfolio.service.client.js
@@ -6,7 +6,7 @@
         .module('PortManApp')
         .factory('PortfolioService', PortfolioService);
 
-    function PortfolioService($http) {
+    function PortfolioService($http, $q) {
 
         function findPortfolioByUserId(userId) {
             return $http.get('/api/project/user/' + userId + '/portfolio');
@@ -28,12 +28,64 @@
             return $http.put('/api/project/user/'+userId+'/portfolio/'+stockId, stock);
         }
 
+        function getPortfolio(userId) {
+            var deferred = $q.defer();
+            findPortfolioByUserId(userId)
+                .then(function(portfolio){
+                    var userStocks = [];
+                    _.forEach(portfolio.data, function(stock) {
+                        var portfolioStock = {
+                            _id: stock._id,
+                            code: stock.code,
+                            invPrice: stock.invPrice,
+                            quantity: stock.quantity,
+                            invDate: moment(stock.invDate).format('MM/DD/YYYY'),
+                            invTotal: Number(stock.invPrice) * Number(stock.quantity)
+                        };
+                        $http({
+                            method: 'GET',
+                            url: 'https://www.quandl.com/api/v3/datasets/WIKI/' + stock.code + '.json?auth_token=bbt3K2NScvyFC4f-trat'
+                        }).then(function successCallback(response) {
+                            portfolioStock.stockIndex = response.data.dataset.data[0][4];
+                            portfolioStock.currentTotal = Number(response.data.dataset.data[0][4]) * Number(stock.quantity);
+                            if (portfolioStock.invTotal <= portfolioStock.currentTotal) {
+                                portfolioStock.profit = portfolioStock.currentTotal - portfolioStock.invTotal;
+                                portfolioStock.profitPercentage = numeral(portfolioStock.profit/portfolioStock.invTotal).format('0.00%');
+                                portfolioStock.profit = numeral(portfolioStock.profit).format('$0,0.00');
+                            } else {
+                                portfolioStock.loss = portfolioStock.invTotal - portfolioStock.currentTotal;
+                                portfolioStock.lossPercentage = numeral(portfolioStock.loss/portfolioStock.invTotal).format('0.00%');
+                                portfolioStock.loss = numeral(portfolioStock.loss).format('$0,0.00');
+                            }
+
+                            portfolioStock.invTotal = numeral(portfolioStock.invTotal).format('$0,0.00');
+                            portfolioStock.currentTotal = numeral(portfolioStock.currentTotal).format('$0,0.00');
+
+                            userStocks.push(portfolioStock);
+
+                            if (userStocks.length == portfolio.data.length) {
+                                deferred.resolve(userStocks);
+                            } else {
+                                deferred.notify(userStocks);
+                            }
+
+                        }, function errorCallback(response) {
+                            console.log(response);
+                            deferred.reject(response);
+                        });
+                    });
+                });
+
+            return deferred.promise;
+        }
+
         return {
             findPortfolioByUserId: findPortfolioByUserId,
             findPortfolioStockByStockIdAndUserId: findPortfolioStockByStockIdAndUserId,
             deletePortfolioStockByIdAndUserId: deletePortfolioStockByIdAndUserId,
             addStockToUserPortfolio: addStockToUserPortfolio,
-            updatePortfolioStockInUser: updatePortfolioStockInUser
+            updatePortfolioStockInUser: updatePortfolioStockInUser,
+            getPortfolio: getPortfolio
         };
     }
 })();
--- public/project/client/views/portfolio/portfolio.controller.js
+++ public/project/client/views/portfolio/portfolio.controller.js
@@ -16,44 +16,15 @@
         showAllStocksForUser();
 
         function showAllStocksForUser() {
-            PortfolioService.findPortfolioByUserId($rootScope.user._id)
-                .then(function(portfolio){
-                    vm.userStocks = [];
-                    _.forEach(portfolio.data, function(stock) {
-                        var portfolioStock = {
-                            code: stock.code,
-                            invPrice: stock.invPrice,
-                            quantity: stock.quantity,
-                            invDate: moment(stock.invDate).format('MM/DD/YYYY'),
-                            invTotal: Number(stock.invPrice) * Number(stock.quantity)
-                        };
-                        $http({
-                            method: 'GET',
-                            url: 'https://www.quandl.com/api/v3/datasets/WIKI/' + stock.code + '.json?auth_token=bbt3K2NScvyFC4f-trat'
-                        }).then(function successCallback(response) {
-                            portfolioStock.stockIndex = response.data.dataset.data[0][4];
-                            portfolioStock.currentTotal = Number(response.data.dataset.data[0][4]) * Number(stock.quantity);
-                            if (portfolioStock.invTotal <= portfolioStock.currentTotal) {
-                                portfolioStock.profit = portfolioStock.currentTotal - portfolioStock.invTotal;
-                                portfolioStock.profitPercentage = numeral(portfolioStock.profit/portfolioStock.invTotal).format('0.00%');
-                                portfolioStock.profit = numeral(portfolioStock.profit).format('$0,0.00');
-                            } else {
-                                portfolioStock.loss = portfolioStock.invTotal - portfolioStock.currentTotal;
-                                portfolioStock.lossPercentage = numeral(portfolioStock.loss/portfolioStock.invTotal).format('0.00%');
-                                portfolioStock.loss = numeral(portfolioStock.loss).format('$0,0.00');
-                            }
-
-                            portfolioStock.invTotal = numeral(portfolioStock.invTotal).format('$0,0.00');
-                            portfolioStock.currentTotal = numeral(portfolioStock.currentTotal).format('$0,0.00');
-
-                            vm.userStocks.push(portfolioStock);
-
-                        }, function errorCallback(response) {
-                            console.log(response);
-                        });
-                    });
+            PortfolioService.getPortfolio($rootScope.user._id)
+                .then(function(portfolio) {
+                    vm.userStocks = portfolio;
                     delete vm.selectedStock;
                     delete vm.selectedStockIndex;
+                }, function(err) {
+                    console.log(err);
+                }, function(progress) {
+                    vm.userStocks = progress;
                 });
         }
 
