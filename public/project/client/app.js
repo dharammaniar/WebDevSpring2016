fb413eab635298974f23fe31805ceacb9d2c009c
Dharam Maniar <dharammaniar@hotmail.com>
2016-04-18 18:55:06 -0400

    Project: Implemented Autocomplete

--- public/project/client/app.js
+++ public/project/client/app.js
@@ -1,5 +1,5 @@
 'use strict';
 
 (function(){
-    angular.module('PortManApp', ['ngRoute', 'textAngular'])
+    angular.module('PortManApp', ['ngRoute', 'textAngular', 'autocomplete'])
 })();
--- public/project/client/views/navigation/navigation.controller.js
+++ public/project/client/views/navigation/navigation.controller.js
@@ -8,13 +8,42 @@
         .module('PortManApp')
         .controller('NavigationController', NavigationController);
 
-    function NavigationController($rootScope, $location, $route, UserService) {
+    function NavigationController($rootScope, $location, $route, UserService, StockService) {
         var vm = this;
+
+        $('#side-menu').metisMenu();
 
         vm.findStock = findStock;
         vm.logout = logout;
 
-        $('#side-menu').metisMenu();
+        vm.updateStocks = updateStocks;
+
+        var allStocks = [''];
+        vm.allStocks = [''];
+        getAllStocks();
+
+        function getAllStocks() {
+            StockService.getAllStocks()
+                .then(
+                    function(response) {
+                        var stocks = response.data;
+                        _.forEach(stocks, function(stock) {
+                            allStocks.push(stock.code + ' : ' + stock.company);
+                        });
+                    },
+                    function(err) {
+                        console.log(err);
+                    }
+                );
+        }
+
+        function updateStocks(searchTerm) {
+            if (searchTerm.length > 0) {
+                vm.allStocks = _.filter(allStocks, function (stock) {
+                    return stock.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0;
+                })
+            }
+        }
 
         function logout() {
             $rootScope.user = null;
@@ -30,12 +59,19 @@
         }
 
         function findStock(searchTerm) {
-            $rootScope.searchTerm = searchTerm;
-            vm.stock = '';
-            if ($location.$$path === '/searchResults') {
-                $route.reload();
+
+            if (searchTerm.indexOf(':') >= 0) {
+                var stockCode = searchTerm.split(':')[0].trim();
+                vm.stock = '';
+                $location.path('/stock/' + stockCode);
             } else {
-                $location.path('/searchResults');
+                $rootScope.searchTerm = searchTerm;
+                vm.stock = '';
+                if ($location.$$path === '/searchResults') {
+                    $route.reload();
+                } else {
+                    $location.path('/searchResults');
+                }
             }
         }
 
